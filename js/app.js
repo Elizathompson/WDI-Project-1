76c8523cfdc16e7aa0b3e2c615bc4bf7c3483c02
Charlotte Morgan <charlotteellenmorgan@yahoo.co.uk>
2019-01-04 15:41:28 +0000

    pacman now transforming based on direction he is facing, food is generating automatically on board but maze is not yet

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ $(() => {
 
   //---------VARIABLES---------
   const $board = $('.gameboard')
+  console.log($board)
   // const $food = $('.food')
   // const $wall = $('.wall')
   // const $pac = $('.pacman')
@@ -13,23 +14,35 @@ $(() => {
 
   $board.attr('data-width', width)
 
+
+
   for(let i = 0; i<width*width; i++) {
     $board.append($('<div id='+ i +' />'))
   }
 
-  const $grid = $('.gameboard div')
-  $grid.eq(pacPosition).addClass('pacman')
+  const $squares = $('.gameboard div')
+  $squares.eq(pacPosition).addClass('pacman')
 
 
   //---------FUNCTIONS---------
+  //function to create maze
+  function makeMaze() {
+    $('#4,#5,#6,#7,#8,#9,#10,#11').addClass('wall')
+  } makeMaze()
+
+  //function to generate food
+  function makeFood() {
+    $squares.addClass('food')
+  } makeFood()
+
 
   //this function runs based on the keystroke. If pacs position on the grid then add 1 to current position of pac. Add the class of pacman to the div at the new position and remove the class of food so that we no longer see the food.
   function movePac() {
     currentPac = currentPac === 3 ? 0 : currentPac + 1
-    $grid.eq(pacPosition)
+    $squares.eq(pacPosition)
       .addClass('pacman')
-      // .removeClass('food')
-      // .removeClass('big-food')
+      .removeClass('food')
+      .removeClass('big-food')
       .attr('data-step', currentPac)
       .attr('data-direction', direction)
   }
@@ -37,7 +50,7 @@ $(() => {
 
   //event listener for key strokes
   $(document).on('keydown', e => {
-    $grid.eq(pacPosition).removeClass('pacman')
+    $squares.eq(pacPosition).removeClass('pacman')
 
     // left 37, up 38, right 39, down 40
     switch(e.keyCode) {
@@ -46,21 +59,26 @@ $(() => {
         direction = 'backward'
       }
         break
-      case 38: if(pacPosition - width >= 0) pacPosition -= width
+      case 38: if(pacPosition - width >= 0) {
+        pacPosition -= width
+        direction = 'up'
+      }
         break
       case 39: if(pacPosition % width < width-1) {
         pacPosition++
         direction = 'forward'
       }
         break
-      case 40: if(pacPosition + width < width*width) pacPosition += width
+      case 40: if(pacPosition + width < width*width){
+        pacPosition += width
+        direction = 'down'
+      }  
         break
     }
 
     movePac()
 
   })
-
 
 
 
